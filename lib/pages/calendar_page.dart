680bfa6154b6c288769d29168b63ddac7ddaa4ca
NizoAT <98343015+NizoAT@users.noreply.github.com>
2022-09-22 12:29:50 +0000

    detail Calandar

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:luxushotels/widgets/myappbar.dart';
 
@@ -6,106 +7,130 @@ DateTime kNow = DateTime.now();
 DateTime kFirstDay = DateTime(kNow.year, kNow.month - 3, kNow.day);
 DateTime kLastDay = DateTime(kNow.year, kNow.month + 3, kNow.day);
 const d_green = Color(0xFF54D3C2);
+ 
 
 class CalendarPage extends StatelessWidget {
-  
+
   const CalendarPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const MyAppBar(),
-      body: Expanded(
-        child: Column(
-          children: [
-            PeriodSection(),
-            CalendarRange(),
-            ValidateBookingSection(),
+      body:  Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        
+          children:  const <Widget>[
+           
+           Expanded(
+            
+              child: PeriodSection(),
+              
+           ),
+           
+            Expanded(
+            
+            child: CalendarRange(),
+            ),
+           
+              Expanded(
+               
+                  child: ValidateBookingSection(),
+                 
+           ),
+            
           ],
         ),
-      ),
+      
     );
   }
 }
 
 class PeriodSection extends StatelessWidget {
+
+  const PeriodSection({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Départ',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
-                    color: Colors.grey[700],
-                  ),
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Arrivée',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                  color: Colors.grey[700],
                 ),
-                SizedBox(
-                  height: 4,
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              const Text(
+                'Sam 12 décembre',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
-                Text(
-                  'Mois 12 décembre',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
+              ),
+            ],
+          ),
+          Container(
+            height: 60,
+            width: 1,
+            color: Colors.grey[350],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Départ',
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                  color: Colors.grey[700],
                 ),
-              ],
-            ),
-            Container(
-              height: 60,
-              width: 1,
-              color: Colors.grey[350],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Revenir',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
-                    color: Colors.grey[700],
-                  ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              const Text(
+                'mar 22 déc',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
-                SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  'mar 22 déc',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-        Divider(
-          color: Colors.grey,
-          height: 1,
-        ),
-      ],
+              ),
+            ],
+          ),
+        ],
+      ),
+      const Divider(
+        color: Colors.grey,
+        height: 1,
+      ),
+    ],
     );
   }
 }
 
 class CalendarRange extends StatefulWidget {
+  const CalendarRange({super.key});
+
   @override
-  _CalendarRangeState createState() => _CalendarRangeState();
+  State<CalendarRange> createState() => _CalendarRangeState();
 }
 
 class _CalendarRangeState extends State<CalendarRange> {
+  
   CalendarFormat _calendarFormat = CalendarFormat.month;
   RangeSelectionMode _rangeSelectionMode = RangeSelectionMode
       .toggledOn; // Can be toggled on/off by longpressing a date
@@ -117,22 +142,21 @@ class _CalendarRangeState extends State<CalendarRange> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(15),
+      padding: const EdgeInsets.all(5),
       color: Colors.grey[10],
-      child: Expanded(
-        child: TableCalendar(
+      child:  TableCalendar(
           lastDay: kLastDay,
           focusedDay: _focusedDay,
           selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
           rangeStartDay: _rangeStart,
           rangeEndDay: _rangeEnd,
           calendarFormat: _calendarFormat,
-          headerStyle: HeaderStyle(
+          headerStyle: const HeaderStyle(
             titleCentered: true,
             formatButtonVisible: false,
             titleTextStyle: TextStyle(fontSize: 18),
           ),
-          calendarStyle: CalendarStyle(
+          calendarStyle: const CalendarStyle(
             isTodayHighlighted: false,
             rangeHighlightColor: d_green,
             rangeStartDecoration: BoxDecoration(
@@ -141,7 +165,7 @@ class _CalendarRangeState extends State<CalendarRange> {
               border: Border.fromBorderSide(
                 BorderSide(
                   color: Colors.white,
-                  width: 3,
+                  width: 2,
                   style: BorderStyle.solid,
                 ),
               ),
@@ -155,7 +179,7 @@ class _CalendarRangeState extends State<CalendarRange> {
               border: Border.fromBorderSide(
                 BorderSide(
                   color: Colors.white,
-                  width: 3,
+                  width: 2,
                   style: BorderStyle.solid,
                 ),
               ),
@@ -194,38 +218,49 @@ class _CalendarRangeState extends State<CalendarRange> {
             _focusedDay = focusedDay;
           },
         ),
-      ),
+      
     );
   }
 }
 
 class ValidateBookingSection extends StatelessWidget {
   final selectedRadio = 1;
+
+  const ValidateBookingSection({super.key});
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        RadioListTile(
+        Expanded(
+        child: RadioListTile(
           value: selectedRadio,
           activeColor: d_green,
           groupValue: selectedRadio,
           selected: true,
-          title: Text(
+          title: const Text(
             "Flexible avec les dates",
-            style: new TextStyle(
+            style: TextStyle(
               color: Colors.black,
               fontSize: 20.0,
             ),
           ),
           onChanged: null,
         ),
-        Container(
+        ),
+      
+      Expanded(
+        child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          width: double.infinity,
+          width: double.infinity /2,
           child: ElevatedButton(
-            child: const Text(
+            child:  Text(
               'Appliquer',
-              style: TextStyle(fontSize: 17),
+              style: GoogleFonts.nunito(
+                fontSize: 17,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+
+              ),
             ),
             style: ElevatedButton.styleFrom(
               backgroundColor: d_green,
@@ -240,6 +275,7 @@ class ValidateBookingSection extends StatelessWidget {
             },
           ),
         ),
+      ),
       ],
     );
   }
